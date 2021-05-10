f15dc66557da4e3639156b9bc4a0c28535b41479
Hansem Ro <hansemro@outlook.com>
2021-05-10 14:02:39 -0700

    Update decoder and word classes

--- decoder.py
+++ decoder.py
@@ -54,58 +54,67 @@ class decoder:
     # @param depth: (default: 16)
     # @param width: (default: 32 (bits))
     # @param topo: (default: 1: "NOR4")
-    def __init__(self, inputs: np.ndarray, depth=16, width=32, topo=1, inv_cap=1*_prefix.get("f"), output_load_cap=None):
+    def __init__(self, inputs: np.ndarray, depth=16, width=32, topo=1, inv_cap=1*_prefix.get("f"), output_load_cap_factor=4*_prefix.get("f")):
         assert int(np.log2(depth)) == inputs.size/2
         self.inputs = inputs
         self.top = solver.top_module(inputs)
         self.depth = depth
         self.width = width
         self.topo = topo
-        if output_load_cap is not None:
-            self.output_load_cap = output_load_cap
-        else:
-            self.output_load_cap = width * 4*_prefix.get("f")
+        inv = False
+        self.output_load_cap = width * output_load_cap_factor
         if topo == 2:
             self.topo_detailed = "NAND4-INV"
         elif topo == 3:
             self.topo_detailed = "NAND2-NOR2"
         elif topo == 4:
             self.topo_detailed = "INV-NAND4-INV"
+            inv = True
         elif topo == 5:
             self.topo_detailed = "NAND4-INV-INV-INV"
         elif topo == 6:
             self.topo_detailed = "NAND2-NOR2-INV-INV"
         else:
             self.topo_detailed = "NOR4"
+            inv = True
         print("topo: ", self.topo_detailed)
         # construct depth number of words as logical units
         for i in range(0,depth):
-            sub_inputs = self.__gen_pattern(i)
+            sub_inputs = self.__gen_pattern(i, inv=inv)
+            print(sub_inputs)
             sub_output = "word" + str(i)
             tmp_word = word(sub_inputs, sub_output, width=width, topo=topo)
+            print(self.__estimate_wire_cap(i))
     
-    def __gen_pattern(self, num):
+    def __gen_pattern(self, num, inv=False):
         assert num >= 0 and self.depth > num
         ret = np.array([])
         tmp = num
         for i in range(0,int(np.log2(self.depth))):
             bit = tmp % 2
             tmp = tmp / 2
-            if int(bit) == 1:
+            if (inv and int(bit) == 0) or (not inv and int(bit) == 1):
                 ret = np.append(ret, self.inputs[i])
             else:
                 ret = np.append(ret, self.inputs[i + int(np.log2(self.depth))])
         return ret
+    
+    def __estimate_wire_cap(self, word_num, wire_cap_per_um = 0.2 * _prefix.get("f"), word_ver_pitch=3.6 * _prefix.get("u")):
+        assert word_num >= 0
+        # 0.2fF/um of wire
+        # word decoder has vertical pitch of 3.6um
+        length = (word_num + 1) * word_ver_pitch
+        return wire_cap_per_um * length
 
 # word class:
 #
 # Example: word9 = (A[3:0] == 3'b1001)
-#            _________
-# A[0] ---> |word_dec |---+ word9
-#~A[1] ---> |width(32)|   |
-#~A[2] ---> |         |  ===c_word=4f*width
-# A[3] ---> |_________|   |
-#                         v
+#            __________
+# A[0] ---> |word_block|---+ word9
+#~A[1] ---> |width(32) |   |
+#~A[2] ---> |g, x      |  ===c_word=4f*width
+# A[3] ---> |__________|   |
+#                          v
 #
 # word_dec logic: A[0]&~A[1]&~A[2]&A[3] = AND4(A[0], ~A[1], ~A[2], A[3])
 #
@@ -115,8 +124,8 @@ class decoder:
 # NAND4-INV             : ~~(A[0]&~A[1]&~A[2]&A[3]) = NAND4(net_nand4, A[0], ~A[1], ~A[2], A[3]); INV(word9, net_nand4)
 # NAND2-NOR2            : ~(~(A[0]&~A[1])+~(~A[2]&A[3])) = NAND2(net_nand2_1, A[0], ~A[1]); NAND2(net_nand2_2, ~A[2], A[3]); NOR2(word9, net_nand2_1, net_nand2_2)
 # INV-NAND4-INV         : INV(B[3:0], {~A[3], A[2], A[1], ~A[0]}); NAND4(net_nand4, B); INV(word9, net_nand4)
-# NAND4-INV-INV-INV     : ~~~~(A[0]&~A[1]&~A[2]&A[3]) = NAND4(net_nand4, A[0], ~A[1], ~A[2], A[3]); INV(net_inv_1, net_nand4); INV(net_inv_2, net_inv_1); INV(word9, net_inv_2);
-# NAND2-NOR2-INV-INV    :
+# NAND4-INV-INV-INV     : Same as NAND4-INV, but with two additional inverters at the end
+# NAND2-NOR2-INV-INV    : Same as NAND2-NOR2, but with two additional inverters at the end
 class word(solver.logical_unit):
 
     # Constructor:
